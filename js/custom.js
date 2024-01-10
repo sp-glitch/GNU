01fd5a42740efea930ff0cdca0a91761cc7ee596
sp-glitch <148739294+sp-glitch@users.noreply.github.com>
2024-01-11 01:28:44 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -16,14 +16,34 @@ $(function () {
     },
   });
 
-  $(".info_btn_box .info_btn>a").on("click", function (e) {
+  // $(".main_link .link_box a").on("click", function (e) {
+  //   e.preventDefault();
+  //   const idx = $(this).index();
+  //   $(this).addClass("on").siblings().removeClass("on");
+  //   eq(idx).show().siblings().hide();
+  // });
+
+  const tabBtn = $(".info_btn_box .info_btn");
+  const tabCont = $(".info_table_wrap table");
+  tabCont.hide().eq(0).show();
+
+  tabBtn.on("click", function (e) {
     e.preventDefault();
-    const idx = $(this).index();
-    $(".info_table_wrap .info_left_table")
-      .eq(idx)
-      .addClass("on")
-      .siblings()
-      .removeClass("on");
+    const index = $(this).index();
+
     $(this).addClass("on").siblings().removeClass("on");
+    tabCont.eq(index).show().siblings().hide();
+  });
+
+  var secondSwiper = new Swiper(".main_best_slide .second-swiper", {
+    slidesPerView: 4,
+    spaceBetween: 16,
+    loop: true,
+
+    navigation: {
+      prevEl: ".swiper-button-pp",
+      nextEl: ".swiper-button-nn",
+    },
+
   });
 });
